175904b33048fd87896d80f61cbd0caddff493f4
ghziirmdhn <129648769+ghziirmdhn@users.noreply.github.com>
2023-06-04 22:12:12 +0700

    Create Page1.php

---
+++ Page1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <title>Tugas Akhir DKP</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Monomaniac+One&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="header">
+    <h1>Mist<span>Fest</span></h1>
+    </div>
+    <div class="Container">
+        <form action="dashboard.php" method="POST">
+            <nav class="Login">
+                <label>Username:</label><br>
+                <input type="text" name="email" class="input-text" required><br>
+                <label>Password:  </label><br>
+                <input type="password" name="password" class="input-text" required><br>
+                <br>
+                <input type="submit" name="submit" value="Submit" class= "submitbutton">
+            </nav>
+        </form>
+    </div>
+    <div class="footer">
+        <a href=""></a>
+    </div>
+</body>
+</html>
+
